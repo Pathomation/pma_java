b9f23628b0f95c6a2608c5e5ab983446a7f41d65
Antreas Gribaviotis <antreasgribas@gmail.com>
2022-07-13 17:27:23 +0300

    Update build scripts

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
 call mvn javadoc:javadoc
 call mvn clean deploy
-ROBOCOPY /MIR .\doc ..\..\..\SDK.docs\pma.java
+ROBOCOPY /MIR .\doc ..\..\..\SDK.docs\pma.java.documentation
 cd ..\..\..\SDK.docs
 svn commit -m "Update from jenkins build" --username Antreas --password Patho!Andre4$
 powershell "Invoke-WebRequest -Uri https://docs.pathomation.com/sdk/update.php -UseBasicParsing"
